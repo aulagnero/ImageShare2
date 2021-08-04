d4ec081a369e94fd5b12e4b768def9f70330dc3e
aulagnero <aulagnero@gmail.com>
2021-08-03 21:03:57 -0400

    thumbnails de foto elegida

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import { StyleSheet, Image, Text, TouchableOpacity, View } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 
 export default function App() {
+  const [selectedImage, setSelectedImage] = React.useState (null);
+
   let openImagePickerAsync = async () => {
     let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
 
@@ -12,8 +14,24 @@ export default function App() {
     }
 
     let pickerResult = await ImagePicker.launchImageLibraryAsync();
-    console.log(pickerResult);
-  }
+
+    if (pickerResult.cancelled === true) {
+      return;
+    }
+
+    setSelectedImage({ localUri: pickerResult.uri });
+  };
+
+if (selectedImage !== null) {
+return (
+  <View style={styles.container}>
+  <Image
+  source={{ uri: selectedImage.localUri }}
+  style={styles.thumbnail}
+  />
+  </View>
+);
+}
 
   return (
     <View style={styles.container}>
@@ -57,5 +75,10 @@ const styles = StyleSheet.create({
   buttonText: {
     fontSize:20,
     color: '#fff',
+  },
+  thumbnail: {
+    width: 300,
+    height: 300,
+    resizeMode: "contain"
   }
 });
